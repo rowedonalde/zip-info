1ad9a238da3b91d6793d9dcfd2cc051f7b546d85
Don Rowe <rowedonalde@gmail.com>
2016-10-07 23:29:36 -0700

    Add support for greater archive detail

--- src/zip_info.rs
+++ src/zip_info.rs
@@ -3,9 +3,33 @@ extern crate zip;
 use std::fs;
 
 fn info_for_archive_item(archive_item: zip::read::ZipFile) -> String {
-    let info = String::new();
+    let mut info = String::new();
     let item_path = archive_item.name();
-    format!("{}\n\t{}", info, item_path)
+    info = format!("{}\n\t{}", info, item_path);
+
+    let compression_type = archive_item.compression();
+    info = format!("{}\n\t\tCompression type: {}", info, compression_type);
+
+    let original_size = archive_item.size();
+    info = format!("{}\n\t\tOriginal size: {}", info, original_size);
+
+    let compressed_size = archive_item.compressed_size();
+    info = format!("{}\n\t\tCompressed size: {}", info, compressed_size);
+
+    if original_size > 0 {
+        let compression_rate =
+            (original_size as f64 - compressed_size as f64)
+            / original_size as f64;
+        let compression_perc = format!("{:.*}%", 2, compression_rate * 100.0);
+        info = format!("{}\n\t\tCompression_rate: {}", info, compression_perc);
+    }
+
+    let comment = archive_item.comment();
+    if comment.len() > 0 {
+        info = format!("{}\n\t\tComment: {}", info, comment);
+    }
+
+    info
 }
 
 fn info_for_path(path: String) -> String {
