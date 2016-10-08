06e09a9d0f6abae80c6759266307cc71dd81fa34
Don Rowe <rowedonalde@gmail.com>
2016-10-07 19:37:14 -0700

    Create a no-op zi binary
    It displays usage info when you say --help, though

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate rustc_serialize;
+extern crate docopt;
+
+use docopt::Docopt;
+
+/// The Docopt usage string
+const USAGE: &'static str = "
+Usage: zi <path>
+       zi --help
+
+zi presents information about Zip archives.
+
+Common options:
+    -h, --help  Show this usage message
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
 }
